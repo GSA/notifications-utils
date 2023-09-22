93a79b0bd581e5bc130b3a19cf6049f3e18cafda
Andrew Shumway <andrew.shumway@gsa.gov>
2023-09-22 10:03:30 -0600

    Remove freezegun import

--- tests/clients/test_redis.py
+++ tests/clients/test_redis.py
@@ -1,11 +1,6 @@
 from notifications_utils.clients.redis import rate_limit_cache_key
 
 
-from notifications_utils.clients.redis import (
-    rate_limit_cache_key,
-)
-
-
 def test_rate_limit_cache_key(sample_service):
     assert rate_limit_cache_key(sample_service.id, "TEST") == "{}-TEST".format(
         sample_service.id
