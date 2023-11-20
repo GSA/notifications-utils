b26f18ff89afd14860f09831620d546a5ed5d56e
Kenneth Kehl <@kkehl@flexion.us>
2023-11-20 09:41:16 -0800

    fix flake 8

--- notifications_utils/template.py
+++ notifications_utils/template.py
@@ -248,8 +248,7 @@ class BaseSMSTemplate(Template):
     def fragment_count(self):
         content_with_placeholders = str(self)
 
-        return len(content_with_placeholders.encode('utf-8'))
-
+        return len(content_with_placeholders.encode("utf-8"))
 
     def is_message_too_long(self):
         """
