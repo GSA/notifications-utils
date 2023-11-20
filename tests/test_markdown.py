5944cf5fa80e514feb081e33097df5e74389cdd9
Beverly Nguyen <bnguyen@b-flexion.local>
2023-11-20 15:19:06 -0800

    updating test

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -7,16 +7,6 @@ from notifications_utils.markdown import (
 )
 from notifications_utils.template import HTMLEmailTemplate
 
-BUTTON_STYLE = (
-    '<button style="font-size: 1.06rem; line-height: 0.9; color: #ffffff; '
-    'background-color: #005ea2; -webkit-appearance: none; -moz-appearance: none; '
-    'appearance: none; border: 0; border-radius: 0.25rem; cursor: pointer; '
-    'display: inline-block; font-weight: 700; margin-right: 0.5rem; '
-    'padding: 0.75rem 1.25rem; text-align: center; text-decoration: none; width: auto;">{}</button>'
-)
-
-LINK_TEXT = "Join Service"
-
 
 @pytest.mark.parametrize(
     "url",
@@ -32,6 +22,16 @@ LINK_TEXT = "Join Service"
     ],
 )
 def test_makes_links_out_of_URLs(url):
+    BUTTON_STYLE = (
+    '<button style="font-size: 1.06rem; line-height: 0.9; color: #ffffff; '
+    'background-color: #005ea2; -webkit-appearance: none; -moz-appearance: none; '
+    'appearance: none; border: 0; border-radius: 0.25rem; cursor: pointer; '
+    'display: inline-block; font-weight: 700; margin-right: 0.5rem; '
+    'padding: 0.75rem 1.25rem; text-align: center; text-decoration: none; width: auto;">{}</button>'
+    )
+
+    LINK_TEXT = "Join Service"
+    
     link = BUTTON_STYLE.format(
             '<a style="word-wrap: break-word; color: #1D70B8;" href="{}">{}</a>'.format(
             url, LINK_TEXT
