662d55472b2063170160c30ae1b20dfdcbff5f1d
Beverly Nguyen <bnguyen@b-flexion.local>
2023-11-20 14:07:54 -0800

    updating test

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -110,7 +110,7 @@ def test_handles_placeholders_in_urls():
         'background-color: #005ea2; -webkit-appearance: none; -moz-appearance: none; '
         'appearance: none; border: 0; border-radius: 0.25rem; cursor: pointer; '
         'display: inline-block; font-weight: 700; margin-right: 0.5rem; '
-        'padding: 0.75rem 1.25rem; text-align: center; text-decoration: none; width: auto;">'      
+        'padding: 0.75rem 1.25rem; text-align: center; text-decoration: none; width: auto;">'
         '<a style="word-wrap: break-word; color: #1D70B8;" href="http://example.com/?token=">'
         "Join Service"
         "</a></button>"
