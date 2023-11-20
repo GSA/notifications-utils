c47340daf34c7d399dc26b4be879b0670e79470b
Beverly Nguyen <bnguyen@b-flexion.local>
2023-11-20 15:35:41 -0800

    updating test

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -62,13 +62,13 @@ def test_makes_links_out_of_URLs(url):
             ("this link is in brackets (http://example.com)"),
             (
                 "this link is in brackets "
-                '<button style="font-size: 1.06rem; line-height: 0.9; color: #ffffff; '
+                '(<button style="font-size: 1.06rem; line-height: 0.9; color: #ffffff; '
                 'background-color: #005ea2; -webkit-appearance: none; -moz-appearance: none; '
                 'appearance: none; border: 0; border-radius: 0.25rem; cursor: pointer; '
                 'display: inline-block; font-weight: 700; margin-right: 0.5rem; '
                 'padding: 0.75rem 1.25rem; text-align: center; text-decoration: none; width: auto;">'
-                '(<a style="word-wrap: break-word; color: #1D70B8;" href="http://example.com">'
-                'Join Service</a>)</button>'
+                '<a style="word-wrap: break-word; color: #1D70B8;" href="http://example.com">'
+                'Join Service</a></button>)'
             ),
         ),
     ],
