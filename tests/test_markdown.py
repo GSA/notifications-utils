f0487d084cec55900d5230f7feb627fcc86a963f
Beverly Nguyen <bnguyen@b-flexion.local>
2023-11-20 15:39:55 -0800

    updating test

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -22,19 +22,16 @@ from notifications_utils.template import HTMLEmailTemplate
     ],
 )
 def test_makes_links_out_of_URLs(url):
-    BUTTON_STYLE = (
-        '<button style="font-size: 1.06rem; line-height: 0.9; color: #ffffff; '
+    LINK_TEXT = "Join Service"
+    link = '<button style="font-size: 1.06rem; line-height: 0.9; color: #ffffff; '
         'background-color: #005ea2; -webkit-appearance: none; -moz-appearance: none; '
         'appearance: none; border: 0; border-radius: 0.25rem; cursor: pointer; '
         'display: inline-block; font-weight: 700; margin-right: 0.5rem; '
-        'padding: 0.75rem 1.25rem; text-align: center; text-decoration: none; width: auto;">{}</button>'
-    )
-    LINK_TEXT = "Join Service"
-    link = BUTTON_STYLE.format(
-        '<a style="word-wrap: break-word; color: #1D70B8;" href="{}">{}</a>'.format(
+        'padding: 0.75rem 1.25rem; text-align: center; text-decoration: none; width: auto;">'
+        '<a style="word-wrap: break-word; color: #1D70B8;" href="{}">{}</a></button>'.format(
             url, LINK_TEXT
         )
-    )
+    
     assert notify_email_markdown(url) == (
         '<p style="Margin: 0 0 20px 0; font-size: 19px; line-height: 25px; color: #0B0C0C;">'
         "{}"
