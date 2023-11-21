661a4f7a0b525a4fc5eb79459df5f918ac77a3cf
Beverly Nguyen <bnguyen@b-flexion.local>
2023-11-21 00:18:07 -0800

    updating test

--- tests/test_template_types.py
+++ tests/test_template_types.py
@@ -553,11 +553,11 @@ def test_makes_links_out_of_URLs_without_protocol_in_sms_and_broadcast(
     template_type,
     url,
     url_with_entities_replaced,
-):
+): 
     expected_link = (
         f"<a "
         f'class="govuk-link govuk-link--no-visited-state" '
-        f'href="{url}">'
+        f'href="http://{url}">'
         f"{url_with_entities_replaced}"
         f"</a>"
     )
